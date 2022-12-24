f9d9a3adc81eb2ab863a54776dddd63b6c230ec6
Wojciech Kwolek <wojciech@kwolek.io>
2022-12-25 00:48:49 +0100

    add ovenmediaengine

---
+++ build-OvenMediaEngine.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DOCKER_ORG="${DOCKER_ORG:-irth7}"
+version="$(cd OvenMediaEngine; git describe --tags)"
+
+docker buildx build \
+    --push \
+    --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+    --tag "$DOCKER_ORG/ovenmediaengine:$version" \
+    ./OvenMediaEngine
